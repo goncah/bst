149fdef1d6837700fec29194adc7e65e2908722f
Hugo Gonçalves <hfdsgoncalves@gmail.com>
2024-05-06 14:32:34 +0100

    Final test set without CAS

--- run_tests.sh
+++ run_tests.sh
@@ -23,18 +23,26 @@
 ./out/bst -o 100000    -g -l -s insert -s write -s read -s read_write -r 10 -t 12
 ./out/bst -o 100000    -g -l -s insert -s write -s read -s read_write -r 10 -t 14
 
-./out/bst -o 1000000 -c -g -l -s insert -s write -s read -s read_write -r 10 -t 2
-./out/bst -o 1000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 4
-./out/bst -o 1000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 6
-./out/bst -o 1000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 8
-./out/bst -o 1000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 10
-./out/bst -o 1000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 12
-./out/bst -o 1000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 14
+./out/bst -o 1000000 -c -g -l -s insert -r 10 -t 2
+./out/bst -o 1000000    -g -l -s insert -r 10 -t 4
+./out/bst -o 1000000    -g -l -s insert -r 10 -t 6
+./out/bst -o 1000000    -g -l -s insert -r 10 -t 8
+./out/bst -o 1000000    -g -l -s insert -r 10 -t 10
+./out/bst -o 1000000    -g -l -s insert -r 10 -t 12
+./out/bst -o 1000000    -g -l -s insert -r 10 -t 14
 
-./out/bst -o 10000000 -c -g -l -s insert -s write -s read -s read_write -r 10 -t 2
-./out/bst -o 10000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 4
-./out/bst -o 10000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 6
-./out/bst -o 10000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 8
-./out/bst -o 10000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 10
-./out/bst -o 10000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 12
-./out/bst -o 10000000    -g -l -s insert -s write -s read -s read_write -r 10 -t 14
+./out/bst -o 10000000 -c -g -l -s insert -r 10 -t 2
+./out/bst -o 10000000    -g -l -s insert -r 10 -t 4
+./out/bst -o 10000000    -g -l -s insert -r 10 -t 6
+./out/bst -o 10000000    -g -l -s insert -r 10 -t 8
+./out/bst -o 10000000    -g -l -s insert -r 10 -t 10
+./out/bst -o 10000000    -g -l -s insert -r 10 -t 12
+./out/bst -o 10000000    -g -l -s insert -r 10 -t 14
+
+./out/bst -o 100000000 -c -g -l -s insert -r 10 -t 2
+./out/bst -o 100000000    -g -l -s insert -r 10 -t 4
+./out/bst -o 100000000    -g -l -s insert -r 10 -t 6
+./out/bst -o 100000000    -g -l -s insert -r 10 -t 8
+./out/bst -o 100000000    -g -l -s insert -r 10 -t 10
+./out/bst -o 100000000    -g -l -s insert -r 10 -t 12
+./out/bst -o 100000000    -g -l -s insert -r 10 -t 14
